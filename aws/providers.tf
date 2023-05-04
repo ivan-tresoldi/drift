eb0656a33fa1de6ce56b6dc27473e1e2b16f6667
ivan-tresoldi <67233434+ivan-tresoldi@users.noreply.github.com>
2023-05-04 20:05:32 +0200

    Create providers.tf

---
+++ aws/providers.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  profile = var.profile
+  region  = var.region
+}
+
+terraform {
+  #backend "s3" {
+  #  bucket = "deployablegoat-test"
+  #  key    = "s3state/deployablegoat"
+  #  region = "us-east-2"
+  #}
+  backend "local" {
+    path = "terraform.tfstate"
+  }
+}
